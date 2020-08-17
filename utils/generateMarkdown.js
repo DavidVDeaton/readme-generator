78756b0d75f57028a7f6da6717f2a8d7e71c5f87
David Deaton <dideater@gmail.com>
2020-08-16 20:38:16 -0700

    first draft

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,45 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+
+    // Variable for badge
+
+    let langBadge = 
+    return `# ${data.title}
+
+# Table of contents
+- [Description](#description)
+- [Language](#language)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Contributing](#Contributing)
+- [Questions](#questions)
+
+# Description
+${data.description}
+# Langurage
+${langBadge}
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# License
+${licenseInfo}
+# Tests
+${data.tests}
+# Contributing
+${data.contributors}
+# Questions
+${data.questions}
+## Creator information  
+---  
+Github profile: https://github.com/${data.username}  
+Contact email: ${data.email}  
+
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
