8f65e8f1f0036331f314d1a736cd2f9cf7fba0ea
David Deaton <dideater@gmail.com>
2020-08-16 22:54:46 -0700

    fixing js bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,45 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-
-    // Variable for badge
-
-
     return `
-    # ${data.title}
-    ---
+      # ${data.title}
+      ---
 
-    # Table of contents
-    - [Description](#description)
-    - [Language](#language)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Tests](#tests)
-    - [Contributing](#Contributing)
-    - [Questions](#questions)
+      # Table of contents
+      - [Description](#description)
+      - [Language](#language)
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Tests](#tests)
+      - [Contributing](#contributing)
+      - [Questions](#questions)
 
-    # Description
-    ${data.description}
-    # Language
-    ${data.language}
-    # Installation
-    ${data.installation}
-    # Usage
-    ${data.usage}
-    # License
-    ${data.license}
-    # Tests
-    ${data.tests}
-    # Contributing
-    ${data.contributors}
-    # Questions
-    ${data.questions}
-    ## Creator information  
-    ---  
-    Github profile: https://github.com/${data.username}  
-    Contact email: ${data.email}  
+      # Description
+      ${data.description}
+      # Language
+      ${data.language}
+      # Installation
+      ${data.installation}
+      # Usage
+      ${data.usage}
+      # License
+      ${data.license}
+      # Tests
+      ${data.tests}
+      # Contributing
+      ${data.contributors}
+      # Questions
+      ${data.questions}
+      ## Creator information  
+      ---  
+      Github profile: https://github.com/${data.username}  
+      Contact email: ${data.email}  
 
-  
   `;
   }
   
